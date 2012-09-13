cd793e94fbc058994f16a58086703ecb8637a570
Gabriel Jacobsohn <you@example.com>
2012-09-13 23:09:19 +0200

    Added an Function Visitor Baseclass for functionbased Metrics

--- PyChara/metricvisitor.py
+++ PyChara/metricvisitor.py
@@ -8,3 +8,15 @@ class MetricVisitor(ast.NodeVisitor):
 
     def continue_parsing(self, node):
         super(MetricVisitor,self).generic_visit(node)
+
+
+class FunctionMetricVisitor(MetricVisitor):
+    """ Computes the Metric function on the and of an Function """
+
+    def compute(self):
+        """
+        Returns the value of an function based metric for one function
+
+        :rtype: int
+        """
+        raise NotImplementedError("Must be implemented")
