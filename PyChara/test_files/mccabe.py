ed29c8376386adcda06ad556ad87f044b495587b
Gabriel Jacobsohn <you@example.com>
2012-09-15 14:46:50 +0200

    Extended the Testfile

--- PyChara/test_files/mccabe.py
+++ PyChara/test_files/mccabe.py
@@ -18,3 +18,31 @@ def a(b):
         print "Be should be function"
     finally:
         return 42
+
+class testClass():
+    def ABC(self, feeling):
+        class av():
+            def ab(self):
+                id =2
+        if (feeling == "good"):
+            print "HEEELLLLOOO World."
+        elif (feeling == "ok"):
+            print "HELLO WORLD"
+        else:
+            print "bye World"
+
+    def feelbarometer(self, feelnumber):
+        def bvc():
+            return 1+1
+        while feelnumber >0:
+            feelnumber -= 1
+        else:
+            feelnumber=0
+
+
+def classy_function():
+    try:
+        print "nnon"
+    except BytesWarning:
+        print "yes yes"
+
